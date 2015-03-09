8d9222a9bdb9b020728d866619097f208c04879c
kevinmachado <machadokevin.dev@gmail.com>
2015-03-09 12:27:09 +0100

    minor adds

---
+++ Fillio/Fillio/network/FIONetworkHTTPServer.swift
@@ -0,0 +1,13 @@
+//
+//  HttpServer.swift
+//  Fillio
+//
+//  Created by Kévin MACHADO on 09/03/2015.
+//  Copyright (c) 2015 Kévin MACHADO. All rights reserved.
+//
+
+import Foundation
+
+class FIONetworkHTTPServer {
+    
+}
