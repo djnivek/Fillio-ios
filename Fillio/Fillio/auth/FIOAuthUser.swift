cc23c0a27b6477506172d1d9c1fa59fdad8734c0
kevinmachado <machadokevin.dev@gmail.com>
2015-03-09 12:14:36 +0100

    Initial hierarchy

---
+++ Fillio/Fillio/auth/FIOAuthUser.swift
@@ -0,0 +1,19 @@
+//
+//  FIOAuthUser.swift
+//  Fillio
+//
+//  Created by Kévin MACHADO on 09/03/2015.
+//  Copyright (c) 2015 Kévin MACHADO. All rights reserved.
+//
+
+import Foundation
+
+class FIOAuthUser {
+    
+    var name: String?
+    var id: String?
+    
+    func currentUser() {
+        
+    }
+}
