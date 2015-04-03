4237a3a31b19453d88062cdf439abf376acf3f59
Kevin MACHADO <machadokevin.dev@gmail.com>
2015-04-03 16:22:29 +0200

    Add Cron Object

---
+++ Fillio/Fillio/cron/FIOCron.swift
@@ -0,0 +1,15 @@
+//
+//  FIOCron.swift
+//  Fillio
+//
+//  Created by Kévin MACHADO on 03/04/2015.
+//  Copyright (c) 2015 Kévin MACHADO. All rights reserved.
+//
+
+import Foundation
+
+class FIOCron {
+    
+    //var manager =
+    
+}
---
+++ Fillio/Fillio/cron/FIOCronManager.swift
@@ -0,0 +1,13 @@
+//
+//  FIOCronManager.swift
+//  Fillio
+//
+//  Created by Kévin MACHADO on 03/04/2015.
+//  Copyright (c) 2015 Kévin MACHADO. All rights reserved.
+//
+
+import Foundation
+
+class FIOCronManager {
+    
+}
