7e24751a3e6c6292d1bd409ccf64a2f42c1da727
Kevin MACHADO <machadokevin.dev@gmail.com>
2015-05-06 14:31:08 +0200

    DIVERS

---
+++ Fillio/Fillio/api/Query.swift
@@ -0,0 +1,17 @@
+//
+//  Query.swift
+//  Fillio
+//
+//  Created by Kévin MACHADO on 05/05/2015.
+//  Copyright (c) 2015 Kévin MACHADO. All rights reserved.
+//
+
+import Foundation
+
+/// This class provides you an easy way to request some
+/// queries from Fillio Server
+class FIOApiQuery {
+    
+    
+    
+}
